85a96143ff215281873f116697fd7a93a833dd6c
Gordon Watts <gordon@gordonandpaula.net>
2019-12-17 08:58:56 -0800

    First go at a test query!
    Working on #1... all the work will occur in other places. :-)

--- tests/test_func_adl_queries.py
+++ tests/test_func_adl_queries.py
@@ -6,17 +6,16 @@ import requests
 # This can take a very long time - 15-30 minutes depending on the quality of your connection.
 # If it is taking too long, most likely the problem is is the downloading - so look at the log
 # from the rucio downloader to track progress (yes, an obvious feature request).
-def test_column_query(running_backend):
+def test_func_adl_query_simple_jets(running_backend):
     'Get electrons using column query'
 
     # Start the request off and get back the basic info about the request.
     response = requests.post(f'{running_backend}/transformation', json={
         "did": "mc15_13TeV:mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.DAOD_STDM3.e3601_s2576_s2132_r6630_r6264_p2363_tid05630052_00",
-        "columns": "Electrons.pt(), Electrons.eta(), Electrons.phi(), Electrons.e(), Muons.pt(), Muons.eta(), Muons.phi(), Muons.e()",
-        "image": "sslhep/servicex-transformer:latest",
+        "columns": "(call ResultTTree (call Select (call SelectMany (call EventDataset (list 'localds://did_01')) (lambda (list e) (call (attr e 'Jets') ''))) (lambda (list j) (call (attr j 'pt')))) (list 'jet_pt') 'analysis' 'junk.root')",
+        "image": "func_adl:latest",
         "result-destination": "object-store",
-        "result-format": "parquet",
-        "chunk-size": 7000,
+        "result-format": "root-file",
         "workers": 1
     })
     print(response.json())
