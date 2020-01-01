eb217f89ed3a8fb2629016b3fa2d8b70f2a3ff58
Gordon Watts <gordon@gordonandpaula.net>
2020-01-01 03:45:01 -0500

    Use the modern version of ServiceX
    Test for selection now works when run locally!

--- tests/servicex_test_utils.py
+++ tests/servicex_test_utils.py
@@ -3,7 +3,6 @@ import requests
 from time import sleep
 from minio import Minio
 import tempfile
-import pyarrow.parquet as pq
 
 def wait_for_request_done(backend_address: str, request_id: str) -> None:
     'Wait until a request has finished processing and files are ready to go'
@@ -32,13 +31,24 @@ def get_servicex_request_data(backend_address: str, request_id: str):
                     secret_key='leftfoot1',
                     secure=False)
     objects = list(minio_client.list_objects(request_id))
-    assert len(objects) == 1
-    sample_file = list([file.object_name for file in objects])[0]
+    assert len(objects) >= 1
 
+    sample_files = list([file.object_name for file in objects])
+    import uproot
+    import uproot_methods
+    import pandas
     with tempfile.TemporaryDirectory() as tmpdirname:
-        f_name = f'{tmpdirname}/sample.root'
-        minio_client.fget_object(request_id,
-                                sample_file,
-                                f_name)
-        pa_table = pq.read_table(f_name)
-    return pa_table
+        def file_to_table(r_id, sample_file_name, output_name):
+            minio_client.fget_object(r_id,
+                                    sample_file_name,
+                                    output_name)
+            f_in = uproot.open(output_name)
+            try:
+                r = f_in[f_in.keys()[0]]
+                assert r is not None
+                return r.pandas.df()
+            finally:
+                f_in._context.source.close()
+
+        all_data = (file_to_table(request_id, s_file, f'{tmpdirname}/sample_{i}.root') for i, s_file in enumerate(sample_files))
+        return pandas.concat(all_data)
--- tests/test_func_adl_queries.py
+++ tests/test_func_adl_queries.py
@@ -7,18 +7,18 @@ import requests
 # If it is taking too long, most likely the problem is is the downloading - so look at the log
 # from the rucio downloader to track progress (yes, an obvious feature request).
 def test_func_adl_query_simple_jets(running_backend):
-    'Get electrons using column query'
+    'Get jet pts using column query'
 
     # Start the request off and get back the basic info about the request.
     response = requests.post(f'{running_backend}/transformation', json={
         "did": "mc15_13TeV:mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.DAOD_STDM3.e3601_s2576_s2132_r6630_r6264_p2363_tid05630052_00",
         "selection": "(call ResultTTree (call Select (call SelectMany (call EventDataset (list 'localds:bogus')) (lambda (list e) (call (attr e 'Jets') 'AntiKt4EMTopoJets'))) (lambda (list j) (/ (call (attr j 'pt')) 1000.0))) (list 'JetPt') 'analysis' 'junk.root')",
-        "image": "servicexxaodcpptransformer:latest",
+        "image": "sslhep/servicex_xaod_cpp_transformer:develop",
         "result-destination": "object-store",
         "result-format": "root-file",
-        "workers": 1
+        "chunk-size": 1000,
+        "workers": 5
     })
-    print(response.json())
     assert response.status_code == 200
     request_id = response.json()["request_id"]
     assert isinstance(request_id, str)
@@ -29,4 +29,5 @@ def test_func_adl_query_simple_jets(running_backend):
     # Load the data back.
     pa_table = get_servicex_request_data(running_backend, request_id)
 
-    assert len(pa_table) == 9800
+    print(pa_table)
+    assert len(pa_table) == 11355980
